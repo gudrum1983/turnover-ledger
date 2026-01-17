ad92f548ad1121daf9c01eb2b6aa04e0abb4225d
Kochkina Ekaterina <gudrum1983@gmail.com>
2026-01-17 14:16:35 +0100

    Создать начальную архитектуру

---
+++ src/app/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import AppHeader from '@/app/AppHeader.vue'
+import ReportMetaForm from '@/widgets/report/ReportMetaForm.vue'
+import ReportActions from '@/widgets/report/ReportActions.vue'
+import ReportTable from '@/widgets/report/ReportTable.vue'
+</script>
+
+<template>
+  <div>
+    <AppHeader msg="Книга КПО (паушал)" />
+    <ReportActions />
+    <ReportMetaForm />
+    <ReportTable />
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/widgets/report/ReportActions.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="ReportActions">
+    <button class="btn-grad">Сформировать отчет</button>
+    <button class="btn-grad2">Экспорт данных</button>
+    <button class="btn-grad">Импорт данных</button>
+  </div>
+</template>
+
+<style scoped></style>
