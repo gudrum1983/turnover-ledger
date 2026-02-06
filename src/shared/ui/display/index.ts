f7fb802f45d26158a1973e89ac3446a71b559e7b
Kochkina Ekaterina <gudrum1983@gmail.com>
2026-02-06 22:33:38 +0100

    Добавить разделитель и разделитель с кнопкой

---
+++ src/shared/ui/display/index.ts
@@ -0,0 +1,4 @@
+export { default as BaseDivider } from './BaseDivider.vue'
+export { default as BaseDividerToggle } from './BaseDividerToggle.vue'
+export { default as BasePaper } from './BasePaper.vue'
+export { default as BaseTag } from './BaseTag.vue'
