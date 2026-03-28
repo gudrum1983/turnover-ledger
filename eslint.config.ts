f03ccdd2170601bbf987e4fa3f704987a75b5a1f
Kochkina Ekaterina <gudrum1983@gmail.com>
2026-03-28 11:16:22 +0100

    Добавить сторибук

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,3 +1,5 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from 'eslint-plugin-storybook'
 import { globalIgnores } from 'eslint/config'
 import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript'
 import pluginVue from 'eslint-plugin-vue'
@@ -17,12 +19,23 @@ export default defineConfigWithVueTs(
 
   // Глобально игнорируем сборочные артефакты.
   globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**']),
-
+  globalIgnores(['!.storybook'], 'Include Storybook Directory'),
   // Базовые правила Vue.
   ...pluginVue.configs['flat/essential'],
+  ...storybook.configs['flat/recommended'],
   // Рекомендованные правила TypeScript для Vue-проекта.
   vueTsConfigs.recommended,
-
+  {
+    rules: {
+      'vue/multi-word-component-names': 'error',
+    },
+  },
+  {
+    files: ['src/stories/**/*.vue'],
+    rules: {
+      'vue/multi-word-component-names': 'off',
+    },
+  },
   // Отключает конфликтующие stylistic-правила ESLint в пользу Prettier.
   skipFormatting,
 )
